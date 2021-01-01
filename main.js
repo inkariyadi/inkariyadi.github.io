7398c5627df7a9486777d2a688522c73f727dc8e
Inka Riyadi <inka.riyadi@gmail.com>
2021-01-01 12:14:08 +0700

    Update main.js

--- main.js
+++ main.js
@@ -35,7 +35,7 @@ function typeWriter() {
 }
 
 
-var countDownDate = new Date("Jan 1, 2021 12:13:00").getTime();
+var countDownDate = new Date("Jan 1, 2021 12:15:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
